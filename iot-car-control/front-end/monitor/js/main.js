e42d28a6fd54e47a0c78eabe6f45ea33d5ef445b
undin0 <118146230+undin0@users.noreply.github.com>
2024-05-28 11:41:10 -0600

    Update main.js

--- iot-car-control/front-end/monitor/js/main.js
+++ iot-car-control/front-end/monitor/js/main.js
@@ -6,7 +6,7 @@ function callApiRequest() {
   // Hacer una petición para un usuario con ID especifico
   axios
     .get(
-      "http://localhost/iot-car-control/back-end/apis/getRegistro.php")
+      "http://3.12.196.65/iot-car-control/back-end/apis/getRegistro.php")
     .then(function (response) {
       // manejar respuesta exitosa
 
